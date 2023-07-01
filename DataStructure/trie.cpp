88f613458207b0ba758eb6018e7d59919a4e8068
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-06-30 23:49:11 -0400

    fix trie

--- DataStructure/trie.cpp
+++ DataStructure/trie.cpp
@@ -6,6 +6,8 @@
 #define vi vector<int>
 #define vll vector<long long>
 
+#define alphabet 26
+
 using namespace std;
 
 class Trie
@@ -15,67 +17,122 @@ private:
     int cant_string_me;
     int cant_node;
     char value;
-    vector<Trie> children;
-
-    int insert(string s, int ind)
-    {
-        cant_string_me++;
-        int cant_new_node = 0;
-
-        if (ind == s.size())
-        {
-            cant_string++;
-            return 0;
-        }
-
-        int find = find_child(s[ind]);
-
-        if (find == -1)
-        {
-            cant_new_node++;
-            children.push_back(Trie(s[ind]));
-            find = children.size() - 1;
-        }
-
-        ind++;
-        int aux = children[find].insert(s, ind);
-        cant_node += (aux + cant_new_node);
-
-        return aux + cant_new_node;
-    }
-
-    int find_child(char a)
-    {
-        int find = -1;
-        for (int i = 0; i < children.size(); i++)
-        {
-            if (children[i].value == a)
-                find = i;
-        }
-
-        return find;
-    }
+    Trie *children[alphabet];
 
 public:
     Trie(char a)
     {
         cant_string = 0;
-        cant_node = 0;
+        cant_node = 1;
         cant_string_me = 0;
         value = a;
+
+        for (int i = 0; i < alphabet; i++)
+            children[i] = nullptr;
     }
 
-    void insert(string s) { insert(s, 0); }
+    pair<Trie *, int> search(string s)
+    {
+        Trie *node = this;
+        int i = 0;
 
-    int cant_words() { return cant_string_me; }
+        while (node->children[s[i] - 'a'] != nullptr && i < s.size())
+        {
+            node = node->children[s[i] - 'a'];
+
+            i++;
+        }
+
+        return {node, i};
+    }
+
+    void insert(string s)
+    {
+        int q = s.size() - search(s).second;
+
+        Trie *node = this;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            node->cant_node += q;
+
+            if (node->children[s[i] - 'a'] == nullptr)
+            {
+                node->children[s[i] - 'a'] = new Trie(s[i]);
+                q--;
+            }
+
+            node = node->children[s[i] - 'a'];
+            node->cant_string_me++;
+        }
+
+        node->cant_string++;
+    }
+
+    void eliminate(string s)
+    {
+        if (!contains(s))
+            return;
+
+        Trie *node = this;
+        int q = 0;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (node->children[s[i] - 'a'] == nullptr)
+            {
+                node->children[s[i] - 'a'] = new Trie(s[i]);
+            }
+
+            if (node->children[s[i] - 'a']->cant_string_me == 1)
+            {
+                node->children[s[i] - 'a'] = nullptr;
+
+                q = s.size() - i;
+                break;
+            }
+
+            node = node->children[s[i] - 'a'];
+            node->cant_string_me--;
+
+            if (i == s.size() - 1)
+                node->cant_string--;
+        }
+
+        node = this;
+
+        for (int i = 0; i < s.size() - q + 1; i++)
+        {
+            node->cant_node -= q;
+            node = node->children[s[i] - 'a'];
+        }
+    }
+
+    bool contains(string s)
+    {
+        auto q = search(s);
+        return q.second == s.size() && q.first->cant_string >= 1;
+    }
+
+    int cant_words_me() { return cant_string_me; }
+
+    int cant_words() { return cant_string; }
+
+    Trie *get(char a) { return children[a - 'a']; }
 
     int size() { return cant_node; }
 };
+
+void solve()
+{
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    solve();
 
     return 0;
 }
