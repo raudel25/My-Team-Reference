b16688285405a7b0a9dfaf2705e1f2c29b514daf
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-10-01 15:20:17 -0400

    update template

--- main.cpp
+++ main.cpp
@@ -1,12 +1,44 @@
 // begin
+// raudel25
 #include <bits/stdc++.h>
 
 #define MAX(a, b) (a > b) ? a : b
 #define MIN(a, b) (a < b) ? a : b
+
 #define int long long
 #define vi vector<int>
 #define pii pair<int, int>
 #define vii vector<pii>
+
+#define sz(x) (int)(x).size()
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define emp emplace
+#define ft front()
+#define bk back()
+#define f first
+#define s second
+
+#define for1(i, a, b) for (int i = ((int)a); i < ((int)b); i++)
+#define for2(i, a) for (int i = 0; i < ((int)a); i++)
+#define for3(i, a, b, c) for (int i = a; i < ((int)b); i += c)
+#define foreach(i, a) for (auto &i : a)
+#define print(a)          \
+    foreach (i, a)        \
+        cout << i << " "; \
+    cout << "\n";
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define yes_no(x) cout << ((x) ? ("YES") : ("NO")) << "\n";
+#define impossible cout << "IMPOSSIBLE\n";
+#define possible cout << "POSSIBLE\n";
 
 using namespace std;
 
@@ -22,10 +54,8 @@ int32_t main()
     int t;
     cin >> t;
 
-    for (int i = 0; i < t; i++)
-    {
+    while (t--)
         solve();
-    }
 
     return 0;
 }
